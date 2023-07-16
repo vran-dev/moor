2c1d7bc2c6955c538fd6c13fb8d595af47458300
vran <vran_dev@foxmail.com>
2023-07-16 12:02:03 +0800

    feat: customize excalidraw menu

--- src/renderer/src/extensions/excalidraw/excalidrawWrapper.tsx
+++ src/renderer/src/extensions/excalidraw/excalidrawWrapper.tsx
@@ -1,4 +1,4 @@
-import { Excalidraw } from '@excalidraw/excalidraw'
+import { Excalidraw, MainMenu } from '@excalidraw/excalidraw'
 import { Attrs } from '@tiptap/pm/model'
 import { Editor, Node, NodeViewWrapper } from '@tiptap/react'
 import { ReactNode, useState, useEffect, useRef } from 'react'
@@ -12,8 +12,8 @@ const ExcalidrawWrapper = (props: {
   const [excalidrawAPI, setExcalidrawAPI] = useState(null)
   const node = props.node
   const attrs = node.attrs
-  const width = attrs.width ? attrs.width : ''
-  const height = attrs.height ? attrs.height : '500px'
+  const width = attrs.width ? attrs.width : '800px'
+  const height = attrs.height ? attrs.height : '600px'
   const initialData = attrs.data ? JSON.parse(attrs.data) : null
   const dataUpdate = (elements, state, files): void => {
     // notice: state.collaborators should be a array ,but export to object
@@ -28,6 +28,37 @@ const ExcalidrawWrapper = (props: {
       props.updateAttributes({ data: newData })
     })
   }
+
+  const toggleZenMode = (): void => {
+    if (excalidrawAPI) {
+      excalidrawAPI.getAppState().zenModeEnabled = !excalidrawAPI.getAppState().zenModeEnabled
+    }
+  }
+
+  const getZenMode = (): boolean => {
+    if (excalidrawAPI) {
+      return excalidrawAPI.getAppState().zenModeEnabled
+    }
+    return false
+  }
+  const toggleGridMode = (): void => {
+    if (excalidrawAPI) {
+      const gridModeEnabled = excalidrawAPI.getAppState().gridModeEnabled
+      if (gridModeEnabled) {
+        excalidrawAPI.getAppState().gridModeEnabled = false
+        excalidrawAPI.getAppState().gridSize = null
+      } else {
+        excalidrawAPI.getAppState().gridModeEnabled = true
+        excalidrawAPI.getAppState().gridSize = 20
+      }
+    }
+  }
+  const getGridMode = (): boolean => {
+    if (excalidrawAPI) {
+      return excalidrawAPI.getAppState().gridModeEnabled
+    }
+    return false
+  }
   return (
     <>
       <NodeViewWrapper
@@ -40,9 +71,31 @@ const ExcalidrawWrapper = (props: {
         <Excalidraw
           initialData={initialData}
           onChange={dataUpdate}
-          zenModeEnabled={false}
+          zenModeEnabled={true}
           ref={(api): void => setExcalidrawAPI(api)}
-        ></Excalidraw>
+        >
+          <MainMenu>
+            <MainMenu.Group>
+              <MainMenu.DefaultItems.LoadScene />
+              <MainMenu.DefaultItems.Export />
+              <MainMenu.DefaultItems.SaveAsImage />
+              <MainMenu.DefaultItems.Help />
+              <MainMenu.DefaultItems.ClearCanvas />
+            </MainMenu.Group>
+            <MainMenu.Group title={'Panel'}>
+              <MainMenu.Item onSelect={toggleZenMode}>
+                Zen Mode {getZenMode() ? '(On)' : '(Off)'}
+              </MainMenu.Item>
+              <MainMenu.Item onSelect={toggleGridMode}>
+                Grid Mode {getGridMode() ? '(On)' : '(Off)'}
+              </MainMenu.Item>
+            </MainMenu.Group>
+            <MainMenu.Group title={'Theme'}>
+              <MainMenu.DefaultItems.ToggleTheme />
+              <MainMenu.DefaultItems.ChangeCanvasBackground />
+            </MainMenu.Group>
+          </MainMenu>
+        </Excalidraw>
       </NodeViewWrapper>
     </>
   )
