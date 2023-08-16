d13adefe07b783d0d0c953041d78bc7f7fd8f658
vran <vran_dev@foxmail.com>
2023-08-16 16:36:13 +0800

    feat: use local excalidraw assets

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './assets/index.css'
 import App from './App'
 
+// @ts-ignore - Excalidraw is not typed
+window.EXCALIDRAW_ASSET_PATH = '/src/assets/'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
