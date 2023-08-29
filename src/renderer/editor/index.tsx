002b3e546e0b9c585c6a9934679465ddd21e55a0
vran <vran_dev@foxmail.com>
2023-08-29 23:02:22 +0800

    feat: create project template

---
+++ src/renderer/editor/index.tsx
@@ -0,0 +1,7 @@
+export function Editor(): JSX.Element {
+  return (
+    <div>
+      <h1>Editor</h1>
+    </div>
+  );
+}
---
+++ src/renderer/ui/Welcome/index.tsx
@@ -0,0 +1,7 @@
+export function Welcome(): JSX.Element {
+  return (
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  );
+}
